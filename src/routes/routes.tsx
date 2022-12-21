ffeafd6737b6f71cd97f44b0f4ef81f9927410f4
marcolucianodev <maurelio.sluciano@gmail.com>
2022-12-21 17:49:04 -0300

    fix build errors

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import About from "../components/About";
-import Contact from "../components/Contact";
 import Home from "../pages/Home";
-import Works from "../components/Works";
-import Modal from "../components/Modal";
 
 const MainRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/:id" element={<Modal />} />
         </Routes>
     )
 }
